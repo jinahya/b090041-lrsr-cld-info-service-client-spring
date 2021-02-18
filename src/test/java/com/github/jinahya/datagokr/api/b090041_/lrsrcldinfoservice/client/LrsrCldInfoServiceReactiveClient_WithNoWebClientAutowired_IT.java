4871159f7c89789075eb47cba8b81229b53bf8d7
Jin Kwon <onacit@users.noreply.github.com>
2021-02-18 19:24:29 +0900

    wip

--- src/test/java/com/github/jinahya/datagokr/api/b090041_/lrsrcldinfoservice/client/LrsrCldInfoServiceReactiveClient_WithNoWebClientAutowired_IT.java
+++ src/test/java/com/github/jinahya/datagokr/api/b090041_/lrsrcldinfoservice/client/LrsrCldInfoServiceReactiveClient_WithNoWebClientAutowired_IT.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.time.Year;
@@ -56,7 +57,9 @@ class LrsrCldInfoServiceReactiveClient_WithNoWebClientAutowired_IT
     @Test
     void getLunCalInfo__() {
         final Year year = Year.now();
-        clientInstance().getLunCalInfo(year, getRuntime().availableProcessors(), Schedulers.parallel())
+        final int parallelism = getRuntime().availableProcessors();
+        final Scheduler scheduler = Schedulers.parallel();
+        clientInstance().getLunCalInfo(year, parallelism, scheduler)
                 .doOnNext(i -> {
                     assertThat(i.getSolarDate()).isNotNull().satisfies(d -> {
                         assertThat(Year.from(d)).isEqualTo(year);
